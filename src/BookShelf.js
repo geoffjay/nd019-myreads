7b89a3337565684ec5ddaa09e5c752863e711381
Geoff Johnson <geoff.johnson@coanda.ca>
2018-02-09 09:08:36 -0800

    refactor: Convert bookshelf component to stateless

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,33 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Book from './Book'
 
-class BookShelf extends Component {
-
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    title: PropTypes.string.isRequired,
-    onChangeBookShelf: PropTypes.func.isRequired
-  }
-
-  render() {
-    const {title, books, onChangeBookShelf} = this.props
-
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.map((book) => (
-              <li key={book.id}>
-                <Book book={book} onChangeBookShelf={onChangeBookShelf} />
-              </li>
-            ))}
-          </ol>
-        </div>
+const BookShelf = (props) => {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.books.map((book) => (
+            <li key={book.id}>
+              <Book book={book} onChangeBookShelf={props.onChangeBookShelf} />
+            </li>
+          ))}
+        </ol>
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+BookShelf.propTypes = {
+  books: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired,
+  onChangeBookShelf: PropTypes.func.isRequired
 }
 
 export default BookShelf
