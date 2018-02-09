99933200783925376506b56c003733a7eb042012
Geoff Johnson <geoff.johnson@coanda.ca>
2018-02-09 11:09:51 -0800

    fix: Display default image when thumbnail missing

--- src/Book.js
+++ src/Book.js
@@ -15,17 +15,27 @@ class Book extends Component {
     return (
       <div className="book">
         <div className="book-top">
-          <div
-            className="book-cover"
-            style={{
-              width: 128,
-              height: 193,
-              backgroundImage: `url("${book.imageLinks.thumbnail}")`
-            }}></div>
+          {book.imageLinks ? (
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url("${book.imageLinks.thumbnail}")`
+              }}></div>
+          ) : (
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: 'url("http://via.placeholder.com/128x193?text=No%20Cover")'
+              }}></div>
+          )}
           <BookShelfChanger book={book} onChangeBookShelf={onChangeBookShelf} />
         </div>
         <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors}</div>
+        <div className="book-authors">{book.authors ? book.authors.join(', ') : ''}</div>
       </div>
     )
   }
