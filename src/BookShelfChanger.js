65d9aa3815574b8601dbe9d67abea480f6acc5f1
Geoff Johnson <geoff.johnson@coanda.ca>
2018-02-08 17:20:52 -0800

    fix: Remove duplicate select option in shelf changer

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -5,7 +5,7 @@ const BookShelfChanger = (props) => {
   return (
     <div className="book-shelf-changer">
       <select value={props.book.shelf} onChange={(e) => props.onChangeBookShelf(props.book, e.target.value)}>
-        <option value="none" disabled>Move to...</option>
+        <option value="move" disabled>Move to...</option>
         <option value="currentlyReading">Currently Reading</option>
         <option value="wantToRead">Want to Read</option>
         <option value="read">Read</option>
